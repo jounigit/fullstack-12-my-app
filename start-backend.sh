a57f4ee840bc15aa520d790f41b98e1973030c94
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2026-06-17 14:01:49 +0300

    docker prod

--- start-backend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start the backend server
-cd backend
-# Database available by docker
-docker compose -f docker-compose.db.yml up -d &&
-# Run backend
-npm run dev 
-echo "backend is running"
